a82fa18cf074e17a3be7b28b228233e2ae7bb89b
Ankit Raj <ankitraj611226@gmail.com>
2026-07-19 14:26:57 +0530

    Add Day 0 Python Basics

---
+++ day0_python_basics.py
@@ -0,0 +1,51 @@
+# ==========================================
+# AI/ML Internship
+# Day 1 - Python Basics
+# Author: Ankit Raj
+# ==========================================
+
+# Print Statement
+print("Welcome to AI/ML Internship")
+print("Hello")
+
+# Variables
+student_name = "Rahul"
+marks = 85
+
+print(student_name)
+print(marks)
+
+# Data Types
+age = 21
+percentage = 89.5
+name = "Aman"
+is_placed = True
+
+print(type(age))
+print(type(percentage))
+print(type(name))
+print(type(is_placed))
+
+# User Input
+student = input("Enter Student Name: ")
+print("Welcome", student)
+
+# Arithmetic Operators
+a = 10
+b = 5
+
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+# Student Marks Analysis
+maths = 80
+science = 75
+english = 90
+
+total = maths + science + english
+percentage = total / 3
+
+print("Total Marks:", total)
+print("Percentage:", percentage)
